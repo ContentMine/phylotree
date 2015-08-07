c2deed9e71a302207ff4d4405f08f7a99989a9f3
Ross Mounce <ross.mounce@gmail.com>
2015-08-07 17:40:51 +0100

    TreeGraph2 validation of ami Newick

---
+++ errors/TreeGraph2-validation-tests/loopover.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+for i in *.nwk ; do 
+		echo processing $i
+		java -jar TreeGraph.jar -convert $i -xtg $i.xtg
+	    	java -jar TreeGraph.jar -image $i.xtg $i.svg -phyl
+		java -jar TreeGraph.jar -image $i.xtg $i.png
+	done
